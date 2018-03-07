bd4d51eb1ea219cb1f214bac70bf425002a9e987
najmath <najmathu8@gmail.com>
2018-03-07 23:50:44 +0530

    front end

---
+++ mas/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import allusers
+admin.site.register(allusers)
+
+# Register your models here.
---
+++ mas/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from .forms import UserForm
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.decorators import login_required
+import pandas as pd 
+def logout_user(request):
+	logout(request)
+	form = UserForm(request.POST or None)
+	context = {
+		"form": form,
+	}
+	return render(request, 'mas/login.html', context)
+
+
+
+
+	
+def login_user(request):
+	if request.user.is_authenticated:
+		return redirect('/')
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			u = User.objects.get(username=username)
+			if user.is_active:
+				login(request, user)
+				return redirect('/')
+			else:
+				return render(request, 'mas/login.html', {'error_message': 'Your account has been disabled'})
+		else:
+			return render(request, 'mas/login.html', {'error_message': 'Invalid login'})
+	return render(request, 'mas/login.html')
+@login_required(login_url='/login')
+def home(request):
+	user = request.user
+	return render(request, 'mas/user.html', {'user':user})
