f545bd9d3f2bcc57a66b73130c9cae4087826fd8
najmath <najmathu8@gmail.com>
2018-03-09 19:19:38 +0530

    template

--- mas/urls.py
+++ mas/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 	url(r'^drmas/$', views.home, name='home'),
     url(r'^login$', views.login_user, name='login_user'),
 	url(r'^$', views.home, name='home'),
-	url(r'^logout_user/$', views.logout_user, name='logout_user'),
-	url(r'^accept/$', views.accept, name='accept'),
+	url(r'^logout_user$', views.logout_user, name='logout_user'),
+	url(r'^accept$', views.accept, name='accept'),
 	]
